1f73e01c41df0494081340b614e673820118c4c1
Nicholas Williams <nicholaswilliams910@gmail.com>
2024-07-16 18:00:02 -0500

    added Navbarcomponent and module with Links. Added matching Routes to App.jsx with appropriate component rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,25 @@
+import { Outlet, Route, Routes } from "react-router-dom"
 import "./App.css"
 import { CustomerList } from "./components/customers/CustomerList"
 import { EmployeeList } from "./components/employees/EmployeeList"
 import { TicketList } from "./components/tickets/TicketList"
+import { Navbar } from "./components/nav/Navbar"
 
 export const App = () => {
 	return (
-		<>
-			{/* <TicketList /> */}
-			{/* <CustomerList /> */}
-			<EmployeeList />
-		</>
+		<Routes>
+			<Route
+				path="/"
+				element={
+					<>
+						<Navbar />
+						<Outlet />
+					</>
+				}>
+				<Route path="tickets" element={<TicketList />} />
+				<Route path="customers" element={<CustomerList />} />
+				<Route path="employees" element={<EmployeeList />} />
+			</Route>
+		</Routes>
 	)
 }
