b87c38c1b4ac3d465486e337065881873e9eb97a
Nicholas Williams <nicholaswilliams910@gmail.com>
2024-07-18 17:02:38 -0500

    added Logout link to Navbar component, which deletes honey_user from local storage and nagivates back to the Login page. added state in ApplicationViews component to store local user so that it may be passed down to Child components as a prop

--- src/views/ApplicationViews.jsx
+++ src/views/ApplicationViews.jsx
@@ -6,8 +6,17 @@ import { CustomerList } from "../components/customers/CustomerList"
 import { CustomerDetails } from "../components/customers/CustomerDetails"
 import { EmployeeList } from "../components/employees/EmployeeList"
 import { EmployeeDetails } from "../components/employees/EmployeeDetails"
+import { useEffect, useState } from "react"
 
 export const ApplicationViews = () => {
+	const [currentUser, setCurrentUser] = useState({})
+
+	useEffect(() => {
+		const localHoneyUser = localStorage.getItem("honey_user")
+		const honeyUserObject = JSON.parse(localHoneyUser)
+		setCurrentUser(honeyUserObject)
+	}, [])
+
 	return (
 		<Routes>
 			<Route
