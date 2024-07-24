a0018adf4a9554bd4c2d67927b5bb28cc51b3adc
Nicholas Williams <nicholaswilliams910@gmail.com>
2024-07-24 13:14:39 -0500

    Route setup for CustomerViews -- home path route, index route

--- src/views/CustomerViews.jsx
+++ src/views/CustomerViews.jsx
@@ -1,3 +1,20 @@
+import { Outlet, Route, Routes } from "react-router-dom"
+import { Navbar } from "../components/nav/Navbar"
+import { Welcome } from "../components/welcome/Welcome"
+
 export const CustomerViews = () => {
-	return <>Customer Views</>
+	return (
+		<Routes>
+			<Route
+				path="/"
+				element={
+					<>
+						{/* <Navbar /> */}
+						<Outlet />
+					</>
+				}>
+				<Route index element={<Welcome />} />
+			</Route>
+		</Routes>
+	)
 }
