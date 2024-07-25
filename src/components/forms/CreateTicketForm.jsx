67799cabd79c7a8ba60c9722ff6cd48b09a9b76c
Nicholas Williams <nicholaswilliams910@gmail.com>
2024-07-25 16:33:39 -0500

    set state for ticket object with default values so that emergency property is automatically set, onChange event functions for description and emergency fieldset inputs

--- src/components/forms/CreateTicketForm.jsx
+++ src/components/forms/CreateTicketForm.jsx
@@ -1,6 +1,9 @@
+import { useState } from "react"
 import "./Form.css"
 
 export const CreateTicketForm = ({ currentUser }) => {
+	const [ticket, setTicket] = useState({ description: "", emergency: false })
+
 	return (
 		<form>
 			<h2>New Service Ticket</h2>
@@ -11,6 +14,11 @@ export const CreateTicketForm = ({ currentUser }) => {
 						type="text"
 						className="form-control"
 						placeholder="Brief description of problem"
+						onChange={(event) => {
+							const ticketCopy = { ...ticket }
+							ticketCopy.description = event.target.value
+							setTicket(ticketCopy)
+						}}
 					/>
 				</div>
 			</fieldset>
@@ -18,7 +26,14 @@ export const CreateTicketForm = ({ currentUser }) => {
 				<div className="form-group">
 					<label>
 						Emergency:
-						<input type="checkbox" />
+						<input
+							type="checkbox"
+							onChange={(event) => {
+								const ticketCopy = { ...ticket }
+								ticketCopy.emergency = event.target.checked
+								setTicket(ticketCopy)
+							}}
+						/>
 					</label>
 				</div>
 			</fieldset>
