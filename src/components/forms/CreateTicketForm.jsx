38cabea763f7ad02324b13d5641843add0e3d1ad
Nicholas Williams <nicholaswilliams910@gmail.com>
2024-07-25 16:24:01 -0500

    jsx for new service ticket form including a text fieldset input, Emergency fieldset checkbox, and a Submit Ticket button

--- src/components/forms/CreateTicketForm.jsx
+++ src/components/forms/CreateTicketForm.jsx
@@ -1,3 +1,32 @@
+import "./Form.css"
+
 export const CreateTicketForm = ({ currentUser }) => {
-	return <>{currentUser.id}</>
+	return (
+		<form>
+			<h2>New Service Ticket</h2>
+			<fieldset>
+				<div className="form-group">
+					<label>Description</label>
+					<input
+						type="text"
+						className="form-control"
+						placeholder="Brief description of problem"
+					/>
+				</div>
+			</fieldset>
+			<fieldset>
+				<div className="form-group">
+					<label>
+						Emergency:
+						<input type="checkbox" />
+					</label>
+				</div>
+			</fieldset>
+			<fieldset>
+				<div className="form-group">
+					<button className="form-btn btn-info">Submit Ticket</button>
+				</div>
+			</fieldset>
+		</form>
+	)
 }
